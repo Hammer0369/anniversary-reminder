e489b650dc79bd9b8c6dafc9fcebe6713fe3c74b
Hammer0369 <akatahama0369@gmail.com>
2021-08-22 10:34:11 +0900

    eventモデルにバリデーションを記述

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :gender, :category, :user 
+
+  with_options presence: true do
+    validates :name, :gender_id, :anniversary_date, :category_id, :anniversary_name
+  end
+
+  validates :gender_id, :category_id, numericality: { other_than: 1 } 
 end

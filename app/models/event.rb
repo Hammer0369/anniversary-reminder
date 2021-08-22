7b7869c6f9295672decf7e4377d511a75da75707
Hammer0369 <akatahama0369@gmail.com>
2021-08-22 10:46:55 +0900

    修正）eventモデルのバリデーション一部
    anniversary_nameはからでも登録できるようにするため削除

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
   belongs_to :gender, :category, :user 
 
   with_options presence: true do
-    validates :name, :gender_id, :anniversary_date, :category_id, :anniversary_name
+    validates :name, :gender_id, :anniversary_date, :category_id
   end
 
   validates :gender_id, :category_id, numericality: { other_than: 1 } 

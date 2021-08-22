90a80ff36602e00745e018685522a9f6d501a7b1
Hammer0369 <akatahama0369@gmail.com>
2021-08-22 10:16:11 +0900

    ActiveHashの各モデル作成とアソシエーション記述

---
+++ app/models/category.rb
@@ -0,0 +1,12 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '誕生日' },
+    { id: 3, name: '結婚記念' },
+    { id: 4, name: '付き合い始めた日' },
+    { id: 5, name: 'その他' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :events
+  end

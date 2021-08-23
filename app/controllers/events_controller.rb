ab78caa012a3178e714874224dde7adb3e6bdd15
Hammer0369 <akatahama0369@gmail.com>
2021-08-23 11:13:16 +0900

    イベント保存機能の実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,26 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
   end
-  
+
   def new
+    @event = Event.new
   end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
+
+private
+
+def event_params
+  params.require(:event).permit(:name, :gender_id, :anniversary_date, :category_id, :anniversary_name)
+                        .merge(user_id: current_user.id)
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,7 @@ class Category < ActiveHash::Base
     { id: 2, name: '誕生日' },
     { id: 3, name: '結婚記念' },
     { id: 4, name: '付き合い始めた日' },
-    { id: 5, name: 'その他' }
+    { id: 5, name: 'その他の記念日' }
   ]
 
   include ActiveHash::Associations

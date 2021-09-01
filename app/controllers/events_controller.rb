9390f9a7419dca79440e430aa4364cc860033d87
Hammer0369 <akatahama0369@gmail.com>
2021-09-01 22:10:42 +0900

    eventコントローラーにedit,updateアクションを定義

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_event, except: [:index, :new, :create]
+  before_action :move_out_of_user, only: [:edit, :update, :destroy]
 
   def index
     @events = Event.order('created_at DESC') # .includes(:user)
@@ -19,8 +21,19 @@ class EventsController < ApplicationController
   end
 
   def show
-    @event = Event.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to event_path
+    else
+      render :edit
+    end
+  end
+
 end
 
 private
@@ -29,4 +42,12 @@ def event_params
   params.require(:event).permit(:last_name, :first_name, :last_name_kana, :first_name_kana,
                                 :gender_id, :anniversary_date, :category_id, :anniversary_name)
         .merge(user_id: current_user.id)
+end
+
+def set_event
+  @event = Event.find(params[:id])
+end
+
+def move_out_of_user
+  redirect_to root_path unless current_user.id == @event.user.id 
 end
